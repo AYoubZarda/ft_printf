87d4ad3b5c16b0279001020e4da6f499c2f23a55
AYoubZarda <zardaayoub1@gmail.com>
2022-11-17 20:57:10 +0100

    ok

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: azarda <azarda@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/15 12:04:18 by azarda            #+#    #+#             */
-/*   Updated: 2022/11/17 16:11:21 by azarda           ###   ########.fr       */
+/*   Updated: 2022/11/17 20:51:30 by azarda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,9 @@ int	check_format(const char *format, va_list pt, int i, int ret)
 		else if (format[i - 1] == '%' && format[i] == 's')
 			ret += ft_putstr(va_arg(pt, char *));
 		else if (format[i - 1] == '%' && format[i] == 'x')
-			ret += ft_putex(va_arg(pt, long));
+			ret += ft_putex(va_arg(pt, unsigned int));
 		else if (format[i - 1] == '%' && format[i] == 'X')
-			ret += ft_putexxa(va_arg(pt, long));
+			ret += ft_putexxa(va_arg(pt, unsigned int));
 		else if (format[i - 1] == '%' && format[i] == 'p')
 			ret += ft_putptr(va_arg(pt, unsigned long int));
 		else if (format[i - 1] == '%' && format[i] == 'd')
@@ -32,7 +32,7 @@ int	check_format(const char *format, va_list pt, int i, int ret)
 			ret += ft_putnbr(va_arg(pt, int));
 		else if (format[i - 1] == '%' && format[i] == 'u')
 			ret += ft_putnbru(va_arg(pt, unsigned int));
-		else if (format[i] == '%' && format[i + 1] == '%')
+		else if (format[i - 1] == '%' && format[i] == '%')
 			ret += ft_putchar('%');
 		else if (format[i] != '%')
 			ret += ft_putchar(format[i]);
@@ -61,7 +61,7 @@ int	ft_printf(const char *format, ...)
 // {
 // 	int i;
 // 	int j;
-// 	i = ft_printf("%p %p\n", 0, 0);
-// 	j = printf("%p %p\n", 0, 0);
+// 	i = ft_printf("%%%%\n");
+// 	j = printf("%%%%\n");
 // 	printf("i = %d     j = %d", i, j);
 // }
--- ft_putex.c
+++ ft_putex.c
@@ -6,13 +6,13 @@
 /*   By: azarda <azarda@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/15 22:37:24 by azarda            #+#    #+#             */
-/*   Updated: 2022/11/17 16:14:15 by azarda           ###   ########.fr       */
+/*   Updated: 2022/11/17 20:31:05 by azarda           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int	ft_calcul(long nb)
+int	ft_calcul(long long int nb)
 {
 	int	len;
 
@@ -32,10 +32,10 @@ int	ft_calcul(long nb)
 	return (len);
 }
 
-int	ft_putex(long nb)
+int	ft_putex(unsigned long nb)
 {
 	long	n;
-	char			*hexa;
+	char	*hexa;
 
 	hexa = "0123456789abcdef";
 	n = nb;
@@ -45,10 +45,10 @@ int	ft_putex(long nb)
 	return (ft_calcul(nb));
 }
 
-int	ft_putexxa(long nb)
+int	ft_putexxa(unsigned long nb)
 {
 	long	n;
-	char			*hexa;
+	char	*hexa;
 
 	hexa = "0123456789ABCDEF";
 	n = nb;
@@ -60,9 +60,11 @@ int	ft_putexxa(long nb)
 
 int	ft_putptr(unsigned long nbr)
 {
+	long int	ret;
+
 	ft_putstr("0x");
-	ft_putex(nbr);
-	return (ft_calcul(nbr) + 2);
+	ret = ft_putex(nbr) + 2;
+	return (ret);
 }
 
 // int main ()
